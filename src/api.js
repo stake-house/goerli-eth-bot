533bb442ceca5a0e3c335c2d152abc137a091a01
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2021-11-30 20:34:20 +0500

    updated cache

--- src/api.js
+++ src/api.js
@@ -1,47 +1,70 @@
 const axios = require('axios');
+const rateLimit = require('axios-rate-limit');
 require('dotenv').config();
 const { ETHERSCAN_API_KEY, ETHERSCAN_API_URL, FAUCET_ADDRESS } = process.env;
 console.log(ETHERSCAN_API_KEY,ETHERSCAN_API_URL);
 
+
 const getBlockNumber = async function(time) {
+    const request = await rateLimit(axios.create(), {maxRequests: 5, perMillisecondss: 1000, maxRPS: 1})
     const url = `${ETHERSCAN_API_URL}?module=block&action=getblocknobytime&timestamp=${time}&closest=before&apikey=${ETHERSCAN_API_KEY}`
-    console.log(time)
-    return (await axios.get(url)).data.result
-    // .then(res => {
-    //         return res.data.result
-    //     }, (error) => {
-    //         console.log(error)
-    //     }
-    // )
-    
-
-    // console.log(res)
+    return (await (request.get(url))).data.result;
+   
+}
+const getTransactions = async function (address, fromBlock) {
+    const request = await rateLimit(axios.create(), {maxRequests: 5, perMillisecondss: 1000, maxRPS: 1})
+    const url = `${ETHERSCAN_API_URL}?module=account&action=txlist&address=${address}&startblock=${fromBlock}&endblock=latest&sort=desc&apikey=${ETHERSCAN_API_KEY}`
+    return (await request.get(url)).data.result;
 }
 
-const getLogs = async function(address, fromBlock) {
-    const url = `${ETHERSCAN_API_URL}?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=latest&address=${address}&apikey=${ETHERSCAN_API_KEY}`
-    return (await axios.get(url)).data.result
-    
+
+modules.export = {
+    checkDeposit: async function(address) {
+        var paidFlag = false;
+        var time = new Date();
+        time.setDate(time.getDate() - 2);
+
+
+        const fromBlock = await getBlockNumber(Math.floor(time.getTime()/1000))
+        var depositedTxArray = []
+        //const sendAddress = "0x49a00b366cf5de47304a9a28bfb2956e29de3228"
+        const tx = await getTransactions(address, fromBlock);
+        if (tx) {
+            for (count = 0; count < tx.length; count++){
+                //faucet address shoudld be = 0x00000000219ab540356cBB839Cbe05303d7705Fa
+                if (tx[count].to === FAUCET_ADDRESS && tx[count].value === '32000000000000000000') {
+                    depositedTxArray.push(tx[count].hash);
+                }
+            }
+        }
+        return depositedTxArray;
+
+    }
 }
+
 
 const init = async function(){
 
     var time = new Date();
     time.setDate(time.getDate() - 2);
-    console.log(time.toDateString())
+    //console.log(time.toDateString())
 
 
     const fromBlock = await getBlockNumber(Math.floor(time.getTime()/1000))
-    console.log(fromBlock)
     
-    const logs  = await getLogs( "0x00000000219ab540356cBB839Cbe05303d7705Fa", fromBlock);
+    //const logs  = await getLogs( "0x00000000219ab540356cBB839Cbe05303d7705Fa", fromBlock);
     // console.log(logs)
-    console.log(logs[0]);
+    const sendAddress = "0x49a00b366cf5de47304a9a28bfb2956e29de3228"
+    const tx = await getTransactions(sendAddress, fromBlock);
+    var depositedTxArray = []
+    console.log(tx);
+    var paidFlag = false;
+    for (count = 0; count < tx.length; count++){
+        //console.log(tx[count].to, tx[count].value);
+        if (tx[count].to === "0x00000000219ab540356cbb839cbe05303d7705fa" && tx[count].value === '32000000000000000000') {
+            depositedTxArray.push(tx[count].hash);
+        }
+    }
+    console.log(depositedTxArray);
 }
 init();
-
-// console.log(res)
-
-/*modules.export = {
-  
-}*/
--- src/cacheDb.js
+++ src/cacheDb.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const {REDIS_PORT, REDIS_HOST, REDIS_KEY} = process.env
 const {createClient} = require('redis');
+const api = require('./api.js');
 // console.log(REDIS_HOST)
 
 (async () => {
@@ -17,13 +18,27 @@ const {createClient} = require('redis');
 module.exports = {
     addToCache: async function(address){
         const time = new Date()
-        await client.set(address, 
-            dailyTime, time, 
-            availableHashes, 
+        await client.set(address, dailycount, weeklyCount, validatedTxs, 
             {
             EX: 604800,
             NX: true
-        })
-    }
+        }
+        
+        )
+    },
+
+    checkWeeklyLimit: async function(address){
+        const weeklyCount = await client.hget(address, weeklyCount);
+        return weeklyCount <= 330;
+    },
+    checkDailyLimit: async function(address){
+        const dailyCount = await client.hget(address, dailyCount);
+        return dailyCount <= 66;
+    },
+    confirmTransaction: async function(address){
+        // await client.hset(address, dailyCount);
+        // await client.hset(address, weeklyCount);
+        // await client.hset(address, validatedTransactions);
+    },
 }
 
